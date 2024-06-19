11b8da50e43edd58c2c270c7e59daeebc4d5c8ba
Victor Daniel <99292901+vdanviel@users.noreply.github.com>
2024-06-19 22:29:09 +0000

    init

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,44 @@
 //configs do react
-import * as React from "react";
+import React, { lazy, Suspense } from 'react';
 import * as ReactDOM from "react-dom/client";
-import "./index.css";
+import { LogoLoading } from "./loading/logoloading.jsx";
+
+//importando o estilo geral (onde tem as diretivas tailwind)..
+import('./styles/general.css')
 
 //configs do router
+//createBrowserRouter - cria um objeto dos caminhos da aplicação
+//RouterProvider - pega a view da rota atual e da display
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
 
-const router = createBrowserRouter([
+/*CHAMANDO COMPONENTES EM UMA LINHA NO createBrowserRouter()..
+
+  para isso eu usarei a função lazy do React que careegar componentes de forma assincrona a parte da promisse do arquivo,
+  o qual ele recupera o export default que é considerado a função do componente
+*/
+const loadComponent = (componentPath) => {
+
+  const Component = lazy(() => import(`${componentPath}`));//aqui ele esta pegando o componente e o tornando assíncrono..
+
+  return (
+    <Suspense fallback={LogoLoading}>{/*aqui estamos usando o componente Suspense que é como se fosse o "loading" do carregamento do assíncrono. oque vai aparecer nesse "loading", esta definido em "fallback="*/}
+      <Component />{/*ele recupera o componente passado no parametro e joga aqui*/}
+    </Suspense>
+  );
+
+};
+
+//criando as rotas..
+const modifiedrouter = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: loadComponent('./main/home.jsx'),
   },
 ]);
 
+//criando o ambiente de display (adicionandoRouterProvider)..
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={modifiedrouter} /> {/*router=(seu objetos de definição de rotas) esse router pede o createBrowserRouter que vc vai usar*/}
   </React.StrictMode>
 );
---
+++ client/src/main/home.jsx
@@ -0,0 +1,10 @@
+const html = (
+  <div className="text-[200px]">
+    <h1>Welcome to metaBlog</h1>
+    <p>Here you can post anything you think!</p>
+  </div>
+)
+
+export default function MyApp() {
+    return html;
+}
